56ee89c302ce1471a1dd3a70d9f46be775f69959
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2021-06-02 19:17:34 +0600

    added the components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+
+import blogs from './images/blogs.png';
+import Form from './components/Form/Form';
+import Posts from './components/Posts/Posts';
+import useStyles from './styles';
 
 function App() {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <Container maxidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography variant="h2" align="center">Blogs</Typography>
+        <img src={blogs} alt="blogs" height="200" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="spece-between" alignment="stretch" spacing={3}>
+            <Grid item xs={12} sm={6}>
+              <Posts></Posts>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Form></Form>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
